eddd94425c052db6607a6c0b9bcedbda501655bb
James <mxteroy@gmail.com>
2019-02-08 01:14:34 -0600

    hi

--- forms.py
+++ forms.py
@@ -5,7 +5,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm): 
     username = StringField('Username', validators=[DataRequired(),  Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(),  Email()])
-    confirm_email = StringField('Confirm Email', validators=[DataRequired(),  Email(), EqualTo('Email')])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
--- wrecktionary.py
+++ wrecktionary.py
@@ -33,13 +33,20 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): 
-        flash(f'Account created for {{form.username.data}}!', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for( 'home' ))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+            if form.email.data =='admin@blog.com' and form.password.data =='password':
+                flash('You have been logged in!', 'success')
+                return redirect(url_for('home'))
+            else: 
+                flash('Login Unsuccessful. Please check username and password', 'danger')
+                
     return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__': 
