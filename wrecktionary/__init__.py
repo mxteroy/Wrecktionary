a03f4f78c6ca91ab33175abf58f5e45e85913c52
James <mxtery@gmail.com>
2019-03-28 23:40:27 -0500

    packaged

--- wrecktionary/__init__.py
+++ wrecktionary/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3fe68ef0176e0ea17ea3db319e2fc9cc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from wrecktionary import routes
--- wrecktionary/routes.py
+++ wrecktionary/routes.py
@@ -1,5 +1,8 @@
-from flask import Flask
-from models import User, Post
+from flask import Flask, render_template, flash, redirect, url_for, request
+from wrecktionary import app, bcrypt, db
+from wrecktionary.forms import LoginForm, RegistrationForm, UpdateAccountForm
+from wrecktionary.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts=[
     {
@@ -28,9 +31,16 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:#if user is logged in, user is redirected to the homepage if they are already log in
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit(): 
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created. You are now able to log!', 'success')
         return redirect(url_for( 'home' ))
     return render_template('register.html', title='Register', form=form)
 
@@ -38,10 +48,30 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-            if form.email.data =='admin@blog.com' and form.password.data =='password':
-                flash('You have been logged in!', 'success')
-                return redirect(url_for('home'))
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('home'))
             else: 
-                flash('Login Unsuccessful. Please check username and password', 'danger')
+                flash('Login Unsuccessful. Please check email and password', 'danger')
 
     return render_template('login.html', title='login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for('account'))
+    image_file = url_for("static", filename='profile_pics/'+current_user.image_file)
+    return render_template('account.html', title="account", image_file = image_file, form = form)
